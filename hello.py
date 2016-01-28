6d11f44f2a2e9d03633a6c4f07d7eaa242020676
cmis <cmis@cmis-hs08.(none)>
2016-01-28 14:34:57 +0700

    made changes

--- hello.py
+++ hello.py
@@ -7,4 +7,16 @@ print -2
 print 0.123
 print "Vanda", "Omaree", "cold"
 print ("Vanda", "Omaree", 15, "Student")
+name = "Vanda"
+print name
+aboutme = "Vanda is cold"
+print aboutme
+animal = "monkey"
+print animal
+food = "ice cream"
+print food
+myage = 15
+brotherage = 17
+meandmybrother = myage + brotherage
+print meandmybrother
 
