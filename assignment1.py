a0376cc111af6c87bf6f471625c3f6f029440274
cmis <cmis@cmis-hs08.(none)>
2016-02-01 09:55:29 +0700

    madechanges

---
+++ assignment1.py
@@ -0,0 +1,24 @@
+myname = "Vanda"
+print myname
+myage= 15.3
+print myage
+myheight= 1.61
+print myheight
+onesquare= 4
+print onesquare
+lengthofrectangle= 12
+print lengthofrectangle
+heightofrectangle= 8
+print heightofrectangle
+ageinmonths= myage * 12 
+print ageinmonths
+yearstolive= 74.19 - myage
+print yearstolive
+heightinfeet= myheight * 3.28084
+print heightinfeet
+averagethaiheight = 1.59
+differenceinheight= myheight - averagethaiheight
+print differenceinheight
+areaofasquare= onesquare * onesquare
+print areaofasquare
+
