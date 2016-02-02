cf297ab9a682f461c36dfd3edc0f8ad49652f529
cmis <cmis@cmis-hs08.(none)>
2016-02-02 11:57:22 +0700

    madechanges

--- assignment1.py
+++ assignment1.py
@@ -1,24 +1,37 @@
-myname = "Vanda"
+myname = "Vanda" # tells my name
 print myname
-myage= 15.3
+myage= 15.3 # tells my age
 print myage
-myheight= 1.61
+myheight= 1.61 # tells my height
 print myheight
-onesquare= 4
+onesquare= 4 # length of 1 square
 print onesquare
-lengthofrectangle= 12
+lengthofrectangle= 12 # tells the lenght of my rectangle
 print lengthofrectangle
-heightofrectangle= 8
+heightofrectangle= 8 # tells the height of my rectangle
 print heightofrectangle
-ageinmonths= myage * 12 
+ageinmonths= myage * 12 # tells my age in months
 print ageinmonths
-yearstolive= 74.19 - myage
+yearstolive= 74.19 - myage # tells how many years I have to live
 print yearstolive
-heightinfeet= myheight * 3.28084
+heightinfeet= myheight * 3.28084 # tells my height in feet
 print heightinfeet
-averagethaiheight = 1.59
-differenceinheight= myheight - averagethaiheight
+averagethaiheight = 1.59 #tells the average Thai girls' height
+differenceinheight= myheight - averagethaiheight #tells the difference of my height and Thai girls' height
 print differenceinheight
-areaofasquare= onesquare * onesquare
-print areaofasquare
+areaofsquare= onesquare * onesquare #tells the area of my square
+print areaofsquare
+rectanglewidth= 5
+print rectanglewidth 
+volumeofacube= lengthofrectangle * heightofrectangle * rectanglewidth
+print volumeofacube
+halfvolumeofcube = volumeofacube /2
+print halfvolumeofcube
+oneninth = 1/9.0
+oneninthofarectangle = areaofsquare * oneninth
+print oneninthofarectangle
+print "Hello. My name is " + myname + "." " I am " + str (myage) + " years old" "." " I am " + str (myheight) + " meters tall or " + str (heightinfeet) + "feet tall." " I have " + str (yearstolive) + " years to live."
+print "Hello.  I have a rectangle with the length of", lengthofrectangle, "and the height of", heightofrectangle,"." "I have a square which has an area of", areaofsquare, "." "I also have a cube with the volume of ", volumeofacube, "." " That means, half the volume of my cube is ", halfvolumeofcube, "."
+winky= " ;) "
+print winky * 10000
 
