efea32957fd8260e6fac4f70f6def2e3143d4cbe
cmis <cmis@cmis-hs08.(none)>
2016-02-09 14:06:58 +0700

    made changes

---
+++ function.py
@@ -0,0 +1,25 @@
+def add(a,b):
+	return a+b
+c= add (3,4)
+print c 
+def sub (x,y):
+	return x-y
+z= sub (5,3)
+print z
+def mul(e,f):
+	return e*f
+g= mul (4,4)
+print g
+def div (j,k):
+	return (j/k)
+L= div (2.0,3)
+print L
+def hours_from_seconds (a):
+    return a / 3600
+print hours_from_seconds (86400)
+def circle_area (a):
+    return 3.14159265359 * (a**2)
+print circle_area (5)
+def sphere_volume (a):
+    return 1.33333333333 * 3.14159265359 * (a**3)
+print sphere_volume (5)
