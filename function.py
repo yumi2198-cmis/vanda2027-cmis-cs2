ecb84d6fec03bf6fb624ab3367d2bd7867b9ffe4
cmis <cmis@cmis-hs08.(none)>
2016-02-11 09:57:31 +0700

    made changes

--- function.py
+++ function.py
@@ -17,9 +17,17 @@ print L
 def hours_from_seconds (a):
     return a / 3600
 print hours_from_seconds (86400)
+import math
 def circle_area (a):
-    return 3.14159265359 * (a**2)
+    return math.pi * (a**2)
 print circle_area (5)
 def sphere_volume (a):
     return 1.33333333333 * 3.14159265359 * (a**3)
 print sphere_volume (5)
+def avg_volume (a, b):
+   
+    return ((1.0/6 * math.pi * a**3) + (1.0/6 * math.pi * b**3)) /2
+print avg_volume (10,20)
+
+    
+
