a5f7346896bc9688511e08c5cf626761d340c63c
cmis <cmis@cmis-hs08.(none)>
2016-02-19 10:40:42 +0700

    made changes

--- function.py
+++ function.py
@@ -24,8 +24,10 @@ def avg_volume (a, b):
    
     return ((1.0/6 * math.pi * a**3) + (1.0/6 * math.pi * b**3)) /2
 
-def area (a,b,c):
-    return math.sqrt(2.75*(2.75-a)*(2.75-b)*(2.75-c))
+def area(a,b,c):
+    s= (a+b+c) / 2 
+    return (s*(s - a) * (s - b) * (s - c)) ** 0.5
+
 
 def right_align (word):
     return (80-len(word))*(" ") + word
@@ -68,9 +70,9 @@ d1 = avg_volume (10,20)
 print d1
 d2 = avg_volume (20,21)
 print d2
-f1= area (1,2,3.5)
+f1= area (1.0,2.0,4.0)
 print f1
-f2= area (1,2,2.5)
+f2= area (1.0,2.0,5.0)
 print f2
 e1= right_align ("Waddup")
 print e1
@@ -80,12 +82,40 @@ g1= center ("Hello")
 print g1
 g2= center ("It's me")
 print g2 
-h=  msg_box ("Hello")
-print h
-h=  msg_box ("I love you!")
-print h
-k= msg_box ("I eat cats!")
-print k 
-k= msg_box ("I eat dogs!")
-print k 
-print msg_box (str())
+h1=  msg_box ("Hello")
+print h1
+h2=  msg_box ("I love you!")
+print h2
+k1= msg_box ("I eat cats!")
+print k1
+k2= msg_box ("I eat dogs!")
+print k2
+print msg_box (str(c1))
+print msg_box (str(c2))
+print msg_box (str(z1))
+print msg_box (str(z2))
+print msg_box (str(g1))
+print msg_box (str(g2))
+print msg_box (str(L1))
+print msg_box (str(L2))
+print msg_box (str(I1))
+print msg_box (str(I2))
+print msg_box (str(a1))
+print msg_box (str(a2))
+print msg_box (str(b1))
+print msg_box (str(b2))
+print msg_box (str(d1))
+print msg_box (str(d2))
+print msg_box (str(f1))
+print msg_box (str(f2))
+print msg_box (str(e1))
+print msg_box (str(e2))
+print msg_box (str(g1))
+print msg_box (str(g2))
+print msg_box (str(h1))
+print msg_box (str(h2))
+print msg_box (str(k1))
+print msg_box (str(k2))
+
+
+
